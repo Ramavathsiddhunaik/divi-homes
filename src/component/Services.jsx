3cb1b438e323dc6ce9717101ac03f79fd85220ce
Ramavathsiddhunaik <Siddhu.naik.dev@gmail.com>
2026-04-27 13:50:01 +0530

    fix image names

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -1,14 +1,14 @@
 function OurServices(){
    let services = [
-    { id: 1, name: "FLOOR CLEANING", image: "/Ourservices/floor.png" },
-    { id: 2, name: "GARDEN CLEANING", image: "/Ourservices/deep.png" },
-    { id: 3, name: "GLASS CLEANING", image: "/Ourservices/glass.png" },
-    { id: 4, name: "BHK CLEANING", image: "/Ourservices/group.png" },
-    { id: 5, name: "SPECIFIC CLEANING", image: "/Ourservices/roomclean.png" },
-    { id: 6, name: "SOFA CLEANING", image: "/Ourservices/room.png" },
-    { id: 7, name: "KITCHEN CLEANING", image: "/Ourservices/kitchen.png" },
-    { id: 8, name: "TOILET CLEANING", image: "/Ourservices/toilet.png" },
-    { id: 9, name: "TERRACE WATER TANKER", image: "/Ourservices/watertank.png" },
+    { id: 1, name: "FLOOR CLEANING", image: "/Ourservices/Floor.png" },
+    { id: 2, name: "GARDEN CLEANING", image: "/Ourservices/Garden.png" },
+    { id: 3, name: "GLASS CLEANING", image: "/Ourservices/Glass.png" },
+    { id: 4, name: "BHK CLEANING", image: "/Ourservices/Bhk.png" },
+    { id: 5, name: "SPECIFIC CLEANING", image: "/Ourservices/Specific.png" },
+    { id: 6, name: "DEEP CLEANING", image: "/Ourservices/Deep.png" },
+    { id: 7, name: "KITCHEN CLEANING", image: "/Ourservices/Kitchen.png" },
+    { id: 8, name: "TOILET CLEANING", image: "/Ourservices/Toilet.png" },
+    { id: 9, name: "TERRACE WATER TANKER", image: "/Ourservices/Watertank.png" },
 ]
 const doubled = [...services, ...services]
     return(
