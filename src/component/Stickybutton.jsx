bcdc9012c0de1b5e5fc1757763668abfd9665470
Ramavathsiddhunaik <Siddhu.naik.dev@gmail.com>
2026-04-28 14:21:02 +0530

    add emailjs and call popup

--- src/component/Stickybutton.jsx
+++ src/component/Stickybutton.jsx
@@ -1,28 +1,3 @@
-// function StickyButtons({ openBooking }) {
-//   const whatsappMessage = `Hello Divi Homes!%0AI need a cleaning service.%0APlease contact me.`;
-  
-//   return (
-//     <div className="sticky-buttons">
-//       <a href="tel:+919177038969" className="sticky-btn">
-//         <img src="/Servicecard/Phoneemoji.png" alt="Call" />
-//       </a>
-//       <a href={`https://wa.me/917093040178?text=${whatsappMessage}`} target="_blank" rel="noreferrer" className="sticky-btn whatsapp-btn">
-//         <img src="/Servicecard/whatsappemoji.png" alt="WhatsApp" />
-//       </a>
-//       <div className="sticky-btn" onClick={() => openBooking()}>
-//         <img src="/Servicecard/bookbtn.png" alt="Cart" />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default StickyButtons;
-
-
-
-
-
-
 import { useState } from 'react';
 import emailjs from '@emailjs/browser';
 
